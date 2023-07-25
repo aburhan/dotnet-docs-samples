ed8904d4eb3e461a6a5d9075f033869a44cd857b
Ameenah Burhan <ameenah.burhan@gmail.com>
2023-07-25 16:14:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 import os
 
 # Get the list of environment variables
-env_vars = os.environ
-
-# Print each environment variable
-for key, value in env_vars.items():
-    print(f'{key}: {value}')
+env_file = os.getenv('GITHUB_ENV')
